91e2d6f439f716d7be98727d3863e902ee66b252
Celina04 <144422146+Celina04@users.noreply.github.com>
2023-09-27 12:47:16 +0200

    Add files via upload

---
+++ bank/app/bank_view.php
@@ -0,0 +1,40 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Bank</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/bank.php" method="post">
+	<label for="kwota">Kwota kredytu: </label>
+	<input id="kwota" type="text" name="k" value="<?php isset($k) ? print($k) :""; ?>" /><br />
+	<label for="id_op">Oprocentowanie kredytu: </label>
+	<input id="id_op" type="text" name="o" value="<?php isset($o) ? print($o) :""; ?>" /><br />
+	<label for="czas">Ilość lat: </label>
+	<input id="czas" type="text" name="c" value="<?php  isset($c) ?  print($c) :""; ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Spłata miesięczna: '.$result . ' złotych.'; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
