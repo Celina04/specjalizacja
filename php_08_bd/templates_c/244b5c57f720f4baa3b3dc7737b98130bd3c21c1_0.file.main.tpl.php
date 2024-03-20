e17dcc2b9a67ad975be931ea19a0674380164421
Celina04 <144422146+Celina04@users.noreply.github.com>
2024-03-20 16:01:56 +0100

    Add files via upload

---
+++ php_08_bd/templates_c/244b5c57f720f4baa3b3dc7737b98130bd3c21c1_0.file.main.tpl.php
@@ -0,0 +1,140 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-03-20 15:22:24
+  from "C:\xampp\htdocs\specjalizacja\php_08_bd\app\views\templates\main.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_65faf120130c30_55380330',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '244b5c57f720f4baa3b3dc7737b98130bd3c21c1' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\specjalizacja\\php_08_bd\\app\\views\\templates\\main.tpl',
+      1 => 1523734570,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_65faf120130c30_55380330 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Aplikacja bazodanowa</title>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+		integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/style.css">
+</head>
+
+<body style="margin: 20px;">
+
+<div class="pure-menu pure-menu-horizontal bottom-margin">
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+personList" class="pure-menu-heading pure-menu-link">Lista</a>
+<?php if (count($_smarty_tpl->tpl_vars['conf']->value->roles) > 0) {?>
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+logout" class="pure-menu-heading pure-menu-link">Wyloguj</a>
+<?php } else { ?>	
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+loginShow" class="pure-menu-heading pure-menu-link">Zaloguj</a>
+<?php }?>
+</div>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_26385405565faf12012bfc8_16828625', 'top');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_75528220765faf120130007_81950664', 'messages');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_106613403465faf1201308b8_77668865', 'bottom');
+?>
+
+
+</body>
+
+</html><?php }
+/* {block 'top'} */
+class Block_26385405565faf12012bfc8_16828625 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ <?php
+}
+}
+/* {/block 'top'} */
+/* {block 'messages'} */
+class Block_75528220765faf120130007_81950664 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+<div class="messages error bottom-margin">
+	<ul>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ul>
+</div>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+<div class="messages info bottom-margin">
+	<ul>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ul>
+</div>
+<?php }?>
+
+<?php
+}
+}
+/* {/block 'messages'} */
+/* {block 'bottom'} */
+class Block_106613403465faf1201308b8_77668865 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ <?php
+}
+}
+/* {/block 'bottom'} */
+}
---
+++ php_08_bd/templates_c/96d6d04f7ce5448a022e6906834ac392d2e5e95f_0.file.LoginView.tpl.php
@@ -0,0 +1,65 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-03-20 16:00:11
+  from "C:\xampp\htdocs\specjalizacja\php_08_bd\app\views\LoginView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_65faf9fbb1baf2_05878169',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '96d6d04f7ce5448a022e6906834ac392d2e5e95f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\specjalizacja\\php_08_bd\\app\\views\\LoginView.tpl',
+      1 => 1523733830,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+  ),
+),false)) {
+function content_65faf9fbb1baf2_05878169 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_187041427865faf9fbb1b614_39569103', 'top');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'top'} */
+class Block_187041427865faf9fbb1b614_39569103 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+login" method="post" class="pure-form pure-form-aligned bottom-margin">
+	<legend>Logowanie do systemu</legend>
+	<fieldset>
+        <div class="pure-control-group">
+			<label for="id_login">login: </label>
+			<input id="id_login" type="text" name="login" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->login;?>
+"/>
+		</div>
+        <div class="pure-control-group">
+			<label for="id_pass">pass: </label>
+			<input id="id_pass" type="password" name="pass" /><br />
+		</div>
+		<div class="pure-controls">
+			<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
+		</div>
+	</fieldset>
+</form>	
+<?php
+}
+}
+/* {/block 'top'} */
+}
